961e21aa5be4a8fd5dfae7071d6570f84ebc9da5
TG@MFJD666 <158172744+Lu6618@users.noreply.github.com>
2024-06-12 15:41:12 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -52,7 +52,7 @@ let proxyhosts = [//本地代理域名池
 	//'ppfv2tl9veojd-maillazy.pages.dev',
 ];
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
-let EndPS = '';//节点名备注内容
+let EndPS = 'TG@MFJD666';//节点名备注内容
 let 协议类型 = 'VLESS';
 let FileName = 'TG@MFJD666';
 let SUBUpdateTime = 6; 
