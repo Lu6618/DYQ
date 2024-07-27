b1a1cfa3007d9514295221bb353b200c5f0ce286
TG@MFJD666 <158172744+Lu6618@users.noreply.github.com>
2024-07-27 18:21:47 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -52,7 +52,7 @@ let proxyhosts = [//本地代理域名池
 	//'ppfv2tl9veojd-maillazy.pages.dev',
 ];
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
-let EndPS = '  ⚡️TG@MFJD666  请勿测速';//节点名备注内容
+let EndPS = '  TG@MFJD666  请勿测速';//节点名备注内容
 let 协议类型 = 'TROJAN';
 let FileName = '';
 let SUBUpdateTime = 6; 
